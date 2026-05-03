f8655921a5340aa7826d4dcfe287fba5bd1a0ec5
karie-son <sonofkarie@gmail.com>
2026-05-03 13:14:46 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,15 +1,14 @@
-// Firebase imports
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 // Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAfU4nLbLjrotVYaLXlB8M6ePM5lu6FfUU",
   authDomain: "gdeh-student-portal.firebaseapp.com",
   projectId: "gdeh-student-portal",
-  storageBucket: "gdeh-student-portal.firebasestorage.app",
+  storageBucket: "gdeh-student-portal.appspot.com",
   messagingSenderId: "376874530975",
   appId: "1:376874530975:web:95e97098fc8708e5b94aaa"
 };
@@ -22,5 +21,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-// Export ONLY services
+// Export
 export { auth, db, storage };
