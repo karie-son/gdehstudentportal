ab8fa249d93af0e29ef72c5e783616a75050b1b3
karie-son <sonofkarie@gmail.com>
2026-05-06 14:50:48 +0300

    Update kenyaData.js

--- kenyaData.js
+++ kenyaData.js
@@ -1,26 +1,28 @@
-export const kenyaData = {
-  "Garissa": {
+const kenyaData = {
+  Garissa: {
     "Garissa Township": ["Waberi", "Galbet", "Township"],
-    "Balambala": ["Danyere", "Jarajila"],
-    "Lagdera": ["Modogashe", "Maalimin"],
-    "Dadaab": ["Dadaab", "Liboi", "Hagadera"],
-    "Fafi": ["Bura", "Dekaharia"],
-    "Ijara": ["Masalani", "Hulugho"]
+    Balambala: ["Danyere", "Jarajila"],
+    Lagdera: ["Modogashe", "Maalimin"],
+    Dadaab: ["Dadaab", "Liboi", "Hagadera"],
+    Fafi: ["Bura", "Dekaharia"],
+    Ijara: ["Masalani", "Hulugho"]
   },
 
-  "Wajir": {
+  Wajir: {
     "Wajir East": ["Wagberi", "Buna", "Korondile"],
     "Wajir West": ["Griftu", "Ademasajide"],
     "Wajir North": ["Bute", "Gurar"],
     "Wajir South": ["Eldas", "Dadaja Bulla"],
-    "Tarbaj": ["Tarbaj", "Elben"]
+    Tarbaj: ["Tarbaj", "Elben"]
   },
 
-  "Mandera": {
+  Mandera: {
     "Mandera East": ["Township", "Neboi", "Khalalio"],
     "Mandera West": ["Takaba", "Lagsure"],
     "Mandera North": ["Rhamu", "Ashabito"],
     "Mandera South": ["Kutulo", "Arabia"],
-    "Banissa": ["Banissa", "Derkhale"]
+    Banissa: ["Banissa", "Derkhale"]
   }
 };
+
+export default kenyaData;
