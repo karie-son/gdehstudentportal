5066ab863679cee74cafd4807c4c515145cb5dff
karie-son <sonofkarie@gmail.com>
2026-05-07 10:35:24 +0300

    Update signin.js

--- signin.js
+++ signin.js
@@ -4,7 +4,7 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const form = document.getElementById("signinForm");
+const form = document.getElementById("loginForm");
 
 form?.addEventListener("submit", async (e) => {
   e.preventDefault();
